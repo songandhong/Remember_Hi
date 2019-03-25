143e461d2eb589ef666c88c96bc387f2377fc5c4
HongEunbeen <vvvv980@naver.com>
2019-03-25 13:11:17 +0900

    listview 완성

---
+++ app/src/main/java/s2017s40/kr/hs/mirim/remember_hi/CheckActivity.java
@@ -0,0 +1,51 @@
+package s2017s40.kr.hs.mirim.remember_hi;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class CheckActivity extends AppCompatActivity {
+    ArrayList<String> items;
+    ArrayAdapter<String> adapter;
+     ListView listview;
+
+    FirebaseDatabase database  = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getInstance().getReference();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check);
+        listview = (ListView) findViewById(R.id.check_list_view);
+        items = new ArrayList<String>() ;
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, items) ;
+
+        listview.setAdapter(adapter);
+
+        myRef.child("CheckList").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot fileSnapshot : dataSnapshot.getChildren()) {
+                    items.add(fileSnapshot.getValue(String.class));
+                }
+                adapter.notifyDataSetChanged();
+            }
+            @Override
+            public void onCancelled(DatabaseError error) {
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/s2017s40/kr/hs/mirim/remember_hi/MainAdapter.java
@@ -0,0 +1,55 @@
+package s2017s40.kr.hs.mirim.remember_hi;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder> {
+    private ArrayList<String> mDataset;
+    private ClickCallback callback;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public TextView mTextView;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            mTextView = (TextView)view.findViewById(R.id.item_main_list_text);
+        }
+    }
+    public MainAdapter(ArrayList<String> myDataset, ClickCallback clickCallback) {
+        mDataset = myDataset;
+        this.callback = clickCallback;
+    }
+    @Override
+    public MainAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_recycler_view_main, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        holder.mTextView.setText(mDataset.get(position));
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callback.onItemClick(position);
+            }
+        });
+    }
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+    public interface ClickCallback {
+        void onItemClick(int position);
+    }
+}
+
+
