a37363c1cc3562443d42761942fe583d1975662a
송승연 <wnzk413@gmail.com>
2019-03-25 22:16:08 +0900

    view 아이디랑 자바파일 연결, WriteDiary String 값들 가져오기

--- app/src/main/java/s2017s40/kr/hs/mirim/remember_hi/ViewDiaryActivity.java
+++ app/src/main/java/s2017s40/kr/hs/mirim/remember_hi/ViewDiaryActivity.java
@@ -2,12 +2,18 @@ package s2017s40.kr.hs.mirim.remember_hi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ViewDiaryActivity extends AppCompatActivity {
-    //이거 완성해서 줘~
+    TextView Title, contents;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_diary);
+
+        Title = findViewById(R.id.viewDiary_dateTitle_textView);
+        contents = findViewById(R.id.viewDiary_contents_textView);
+
     }
 }
--- app/src/main/java/s2017s40/kr/hs/mirim/remember_hi/WriteDiaryActivity.java
+++ app/src/main/java/s2017s40/kr/hs/mirim/remember_hi/WriteDiaryActivity.java
@@ -2,20 +2,27 @@ package s2017s40.kr.hs.mirim.remember_hi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class WriteDiaryActivity extends AppCompatActivity {
     Spinner weatherSpinner, emotionSpinner;
     EditText writeDiaryEdit;
     Button writeBtn;
-    //승연아 이거 다 연결해줘~ 그냥 String 선언해서 하나한 하면 될 거 같다! -> 내가 DiaryDTO추가해 놓았으니까 참고 ㄱㄱ
+    TextView dateTitle;
+    String nowTimeStr;
+
     //파이어베이스 연결 위한 준비
     FirebaseDatabase database  = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getInstance().getReference();
@@ -24,6 +31,9 @@ public class WriteDiaryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_diary);
+
+        //일기 맨 상단의 제목
+        dateTitle = findViewById(R.id.writeDiary_titleDay_text);
 
         //글쓰기 버튼
         writeBtn = findViewById(R.id.writeDiary_write_btn);
@@ -35,14 +45,29 @@ public class WriteDiaryActivity extends AppCompatActivity {
         weatherSpinner = findViewById(R.id.writeDiary_weather_spinner);
         emotionSpinner = findViewById(R.id.writeDiary_emotion_spinner);
 
+        long nowTime = System.currentTimeMillis();
+
+        Date date = new Date(nowTime);
+        SimpleDateFormat formatTime = new SimpleDateFormat("yyyy년 MM월 dd일");
+        nowTimeStr = formatTime.format(date);
+
+        dateTitle.setText(nowTimeStr);
+
 
         writeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //글쓰기 버튼을 누르면 스피너의 값을
                 //가져와서 String에 저장
-                String weatherStr = weatherSpinner.getSelectedItem().toString();
-                String emotionStr = emotionSpinner.getSelectedItem().toString();
+
+                String weatherStr = "오늘의 날씨는 " + weatherSpinner.getSelectedItem().toString();
+                String emotionStr = "내 기분은 " + emotionSpinner.getSelectedItem().toString();
+                String editContents = writeDiaryEdit.getText().toString();
+
+                DiaryDTO drDto = new DiaryDTO(editContents, nowTimeStr, emotionStr,
+                        "key?", "key2", "key3", weatherStr);
+
+
             }
         });
     }
