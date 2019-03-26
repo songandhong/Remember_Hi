abd9e791a965b64f104e2ce6975e71b775411158
Seungyeon Song <song99pro@gmail.com>
2019-03-26 09:27:57 +0900

    자잘한 xml 수정

--- app/src/main/java/s2017s40/kr/hs/mirim/remember_hi/ViewDiaryActivity.java
+++ app/src/main/java/s2017s40/kr/hs/mirim/remember_hi/ViewDiaryActivity.java
@@ -5,14 +5,17 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class ViewDiaryActivity extends AppCompatActivity {
-    TextView Title, contents;
+    TextView yearTitle, monthTitle, dateTitle, contents;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_diary);
 
-        Title = findViewById(R.id.viewDiary_dateTitle_textView);
+        yearTitle = findViewById(R.id.viewDiary_YearTitle_textView);
+        monthTitle = findViewById(R.id.viewDiary_monthTitle_textView);
+        dateTitle = findViewById(R.id.viewDiary_dateTitle_textView);
+
         contents = findViewById(R.id.viewDiary_contents_textView);
 
     }
