692aa05fa4841f02ace4f0790b3532e2d68b35ef
송승연 <wnzk413@gmail.com>
2019-03-28 16:01:46 +0900

    local

--- app/src/main/java/s2017s40/kr/hs/mirim/remember_hi/AddMissionActivity.java
+++ app/src/main/java/s2017s40/kr/hs/mirim/remember_hi/AddMissionActivity.java
@@ -35,7 +35,7 @@ public class AddMissionActivity extends AppCompatActivity {
                 String missionMinute = Minute + "";
 
                 MissionDTO mDTO = new MissionDTO(missionTitle, missionHour, missionMinute, 0);
-                
+
             }
         });
         cancelBtn.setOnClickListener(new View.OnClickListener() {
